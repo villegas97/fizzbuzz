3bbd9aad8e81b2a68c53b2904a36739d35ed942a
Luis Alberto Villegas Valenzuela <lualviva97@gmail.com>
2022-05-01 18:34:57 -0500

    Agregado console.log

--- app.js
+++ app.js
@@ -73,3 +73,4 @@ const explorersInNodeAndFizzBuzzTrick = explorersInNode.map((explorer) =>
 );
 
 // Part 8: Get a list of the explorers in node, if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, if is just divisible by 5 set the property trcik and the value BUZZ, if is just divisible by 3 set the property trick and the value FIZZ, otherwise set the property trick and the score value. TODO
+console.log(explorersInNodeAndFizzBuzzTrick);
