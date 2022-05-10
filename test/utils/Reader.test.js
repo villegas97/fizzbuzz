b54daadea42d05c315975307b238a0554f05bfe1
Luis Alberto Villegas Valenzuela <lualviva97@gmail.com>
2022-05-09 19:49:15 -0500

    Corrigiendo la prueba para que pase en el action

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -2,6 +2,6 @@ const Reader = require("./../../lib/utils/Reader");
 describe("Unit Test for Reader Class", () => {
   test("1)Testing JSON parse", () => {
     const explorers = Reader.readJsonFile("explorers.json");
-    expect(explorers[0].name).toBe("Woopa");
+    expect(explorers[0].name).toBe("Woopa1");
   });
 });
